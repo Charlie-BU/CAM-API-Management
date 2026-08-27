aab2d5252121cf7216d0ff96564511cef5efb390
Charlie <charlie@airjelly.ai>
2026-08-27 16:39:52 +0800

    fix(FE-CAM): use fix bytedance npm package @cloud-materials/common issue

--- FE-CAM/vite.config.ts
+++ FE-CAM/vite.config.ts
@@ -6,12 +6,18 @@ import { fileURLToPath, URL } from "node:url";
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "VITE_");
     const PORT = Number(env.VITE_FE_PORT) || 9000;
+    const cloudMaterialsPath = fileURLToPath(
+        new URL("./cloud-materials-common/@cloud-materials/common", import.meta.url)
+    );
+
     return {
         plugins: [react()],
         resolve: {
             alias: {
                 "@": fileURLToPath(new URL("./src", import.meta.url)),
+                "@cloud-materials/common": cloudMaterialsPath,
             },
+            dedupe: ["react", "react-dom"],
         },
         server: {
             port: PORT,
